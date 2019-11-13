d384378a0fba7d9227102b425b0c5acfb470faa6
ryo.watanabe <ryo.watanabe@nijibox.co.jp>
2019-11-13 19:24:32 +0900

    ナウい書き方に大幅変更

--- src/app.js
+++ src/app.js
@@ -1,47 +1,56 @@
 import $ from 'jquery';
 
 $(() => {
-  /**
-   * リストにアイテムを追加
-   */
-  const addListItem = () => {
-    const $addButton = $('.js-addButton');
-    const $list = $('.js-list');
-    let itemNum = 1;
 
-    $addButton.click(() => {
-      const item = `
-      <li class="list__item">
-        <input type="checkbox" class="checkBox js-checkBox">
+  /**
+   * セレクタ
+   */
+  const $list = $('[data-js=list]');
+
+  // 以下はjQueryセレクタではないので注意
+  const listItem = '[data-js=listItem]';
+  const addButton = '[data-js=addButton]';
+  const removeButton = '[data-js=removeButton]';
+  const checkBox = '[data-js=checkBox]';
+
+  // アイテム番号 handleClickAddが発火する度に1増える
+  let itemNum = 1;
+
+  /**
+   * イベントの発火時に実行する関数
+   */
+  const handleClickAdd = () => {
+    const item = `
+      <li class="list__item" data-js="listItem">
+        <input type="checkbox" class="checkBox" data-js="checkBox">
         <p>ITEM${itemNum}</p>
-        <button class="removeButton js-removeButton">remove</button>
+        <button class="removeButton" data-js="removeButton">remove</button>
       </li>
       `;
-      $list.append(item);
-      itemNum += 1;
-    });
+
+    $list.append(item);
+    itemNum += 1;
+  };
+
+  const handleClickRemove = (event) => {
+    const $target = $(event.target);
+    $target.closest(listItem).remove();
+  };
+
+  const handleChangeCheckBox = (event) => {
+    const $target = $(event.target);
+    if ($target.prop('checked')) {
+      $target.closest(listItem).addClass('checked');
+    } else {
+      $target.closest(listItem).removeClass('checked');
+    }
   };
 
   /**
-   * 動的に追加されるアイテムにイベントを追加
+   * 各DOM要素にイベントを設定
    */
-  const addListItemEvent = () => {
-    // 削除ボタン、チェックボックスのイベントをdocumentに対してonする
-    // documentは最初から必ず存在するので、**現在その要素が存在するか**関係なく設定できる
-    $(document).on('click', '.js-removeButton', () => {
-      const $target = $(event.target);
-      $target.parent().remove();
-    })
-    .on('change', '.js-checkBox', () => {
-      const $target = $(event.target);
-      if ($target.prop('checked')) {
-        $target.parent().addClass('checked');
-      } else {
-        $target.parent().removeClass('checked');
-      }
-    });
-  };
-
-  addListItem();
-  addListItemEvent();
+  $('body')
+    .on('click', addButton, handleClickAdd)
+    .on('click', removeButton, handleClickRemove)
+    .on('change', checkBox, handleChangeCheckBox);
 });

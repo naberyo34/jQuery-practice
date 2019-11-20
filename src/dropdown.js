8dae579f0dca68c0253ca6bd6c58b09f89011631
ryo.watanabe <ryo.watanabe@nijibox.co.jp>
2019-11-20 17:08:58 +0900

    #1 ドロップダウン実装

---
+++ src/dropdown.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+
+function dropdown() {
+  /**
+   * セレクタ
+   */
+
+  const $body = $('body');
+
+  const column = '[data-js=dropdown__menuColumn]';
+  const itemList = '[data-js=dropdown__itemList]';
+  const isActive = 'dropdown__itemList--isActive';
+
+  /**
+   * ホバーイベント
+   */
+  const handleMouseEnter = (event) => {
+    const $target = $(event. currentTarget);
+    $target.find(itemList).addClass(isActive);
+    console.log($target);
+  };
+
+  const handleMouseLeave = () => {
+    const $target = $(event.currentTarget);
+    $target.find(itemList).removeClass(isActive);
+    console.log($target);
+  };
+
+  /**
+   * イベントの設定
+   */
+  $body
+    .on('mouseenter', column, handleMouseEnter)
+    .on('mouseleave', column, handleMouseLeave);
+}
+
+export default dropdown;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import $ from 'jquery';
 import hamburger from './hamburger';
+import dropdown from './dropdown';
 import todoList from './todoList';
 import './css/reset.css';
 import './css/index.scss';
 
+
 $(() => {
   hamburger();
+  dropdown();
   todoList();
 });
